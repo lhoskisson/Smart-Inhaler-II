a979eb9fb61f79a2ed7c4574e47f9371762d0dff
lhoskisson <loganhoskisson@gmail.com>
2022-05-07 17:20:21 -0700

    changing queue implementation

--- Inhaler/src/inhaler/iueQueue.cpp
+++ Inhaler/src/inhaler/iueQueue.cpp
@@ -2,41 +2,62 @@
 
 iueQueue:iueQueue()
 {
-  queueSize = 0;
+  initializeHeadTail();
 }
 
 void iueQueue::enqueue(IUE_t iue)
 {
   char[IUE_FILENAME_MAX_LENGTH] fileName;
-  i_to_cstr(queueSize++, fileName, IUE_FILENAME_MAX_LENGTH);
+  i_to_cstr(tail++, fileName, IUE_FILENAME_MAX_LENGTH);
+#ifdef INHALER_SERIAL_ON
+  Serial.print("enqueuing ");
+  Serial.print(fileName);
+  Serial.print(" with ");
+  Serial.println(iue.timestamp);
+#endif
+  noInterrupts();
+  if(fs.exists(fileName))
+    fs.remove(fileName);
   File iueFile = fs.open(fileName, FILE_WRITE);
   iueFile.print(iue.timestamp);
   iueFile.close();
-  updateQueueSizeFile();
+  interrupts();
+  updateQueueTailFile();
 }
 
 IUE_t iueQueue::dequeue()
 {
   IUE_t iue;
-  iueQueueFile.open(IUE_QUEUE_FILE_NAME, FILE_WRITE);
-  long retVal = 0;
-  if(iueQueueFile.available())
+  iue.timestamp = 0;
+  char[IUE_FILENAME_MAX_LENGTH] fileName;
+  i_to_cstr(head++, fileName, IUE_FILENAME_MAX_LENGTH);
+  if(fs.exists(fileName))
   {
-    retVal = iueQueueFile.parseInt();
-    
+    noInterrupts();
+    File iueFile = fs.open(fileName, FILE_READ);
+    iue.timestamp = iueFile.parseInt();
+    iueFile.close();
+    fs.remove(fileName);
+    interrupts();
   }
-  
 #ifdef INHALER_SERIAL_ON
   if(sizeof(long) != sizeof(IUE_t)) Serial.println("IUE Size Mismatch from file");
+  Serial.print("dequeued ");
+  Serial.print(iue.timestamp);
+  Serial.print(" from ");
+  Serial.println(fileName);
 #endif
-
-  iue.timestamp = retVal;
+  if(head == tail)
+  {
+    head = 0;
+    tail = 0;
+  }
   return iue;
 }
 
 int16_t iueQueue::getQueueSize()
 {
-  return _queueSize();
+  return tail - head;
 }
 
 //converts len-1 digits (lsd to msd) of an integer n to a c-string s
@@ -63,8 +84,96 @@ void iueQueue::i_to_cstr(int16_t n, char* s, const int len)
     s[i] = (n % mod) + 48; //48 is ascii for '0'
     mod /= 10;
   }
+#ifdef INHALER_SERIAL_ON
+  Serial.print("i_to_cstr converted int ");
+  Serial.print(n);
+  Serial.print(" to cstr ");
+  Serial.println(s);
+#endif
 }
 
-void iueQueue::updateQueueSizeFile()
+void iueQueue::updateHeadFile()
 {
+  #ifdef INHALER_SERIAL_ON
+  Serial.print("updating head file to ");
+  Serial.println(head);
+#endif
+  noInterrupts();
+  if(fs.exists(HEAD_FILENAME))
+    fs.remove(HEAD_FILENAME);
+  File headFile = fs.open(HEAD_FILENAME, FILE_WRITE);
+  headFile.print(head);
+  headFile.close();
+  interrupts();
+}
+
+int16_t iueQueue::getHeadFromFile()
+{
+  noInterrupts();
+  if(!fs.exists(HEAD_FILENAME))
+    return 0;
+  File headFile = fs.open(HEAD_FILENAME, FILE_READ);
+  int16_t _head = queueSizeFile.parseInt();
+  headFile.close();
+  interrupts();
+#ifdef INHALER_SERIAL_ON
+  Serial.print("retreiving from head file: ");
+  Serial.println(_head);
+#endif  
+  return _head;
+}
+
+void iueQueue::updateTailFile()
+{
+#ifdef INHALER_SERIAL_ON
+  Serial.print("updating tail file to ");
+  Serial.println(tail);
+#endif
+  noInterrupts();
+  if(fs.exists(TAIL_FILENAME))
+    fs.remove(TAIL_FILENAME);
+  File tailFile = fs.open(TAIL_FILENAME, FILE_WRITE);
+  tailFile.print(tail);
+  tailFile.close();
+  interrupts();
+}
+
+int16_t iueQueue::getTailFromFile()
+{
+  noInterrupts();
+  if(!fs.exists(TAIL_FILENAME))
+    return 0;
+  File tailFile = fs.open(TAIL_FILENAME, FILE_READ);
+  int16_t _tail = queueSizeFile.parseInt();
+  tailFile.close();
+  interrupts();
+#ifdef INHALER_SERIAL_ON
+  Serial.print("retreiving from tail file: ");
+  Serial.println(_tail);
+#endif  
+  return _tail;
+}
+
+void iueQueue::initializeHeadTail()
+{
+  head = getHeadFromFile();
+  tail = getTailFromFile();
+
+  if(head == 0 && tail ==0)
+    return;
+
+  if(head < tail)
+  {
+    //check for files above and below head and tail
+  }
+  
+  if(head == tail)
+  {
+    //check for files above and below
+  }
+  
+  if(head > tail)
+  {
+    //check for files up to tail to see where head should be  
+  }
 }
--- Inhaler/src/inhaler/iueQueue.h
+++ Inhaler/src/inhaler/iueQueue.h
@@ -4,9 +4,12 @@
 #include "IUE_t.h"
 #include <SdFat.h>
 
-#define IUE_QUEUE_FILE_NAME "iueQueue.csv"
-#define IUE_FILENAME_MAX_LENGTH (6) //files are named as 16-bit integers, 5 is the most digits a 16-bit int will have. 5 digits + null terminator = 6
+//name of files that will hold queue head and tail in case of power loss
+#define HEAD_FILENAME "head"
+#define TAIL_FILENAME "tail"
 
+//files are named as 16-bit integers, 5 is the most digits a 16-bit int will have. 5 digits + null terminator = 6
+#define IUE_FILENAME_MAX_LENGTH (6)
 
 class iueQueue
 {
@@ -14,20 +17,22 @@ class iueQueue
   iueQueue();
   void enqueue(IUE_t);
   IUE_t dequeue();
-  int getQueueSize();
+  int16_t size();
   
   private:
   FatFileSystem fs;
   
-  //file where queue size is stored in case of device shutdown during transfer of queue to app
-  File queueSize; 
-
+  int16_t head;
   
-  int16_t _queueSize;
+  int16_t tail;
 
   void i_to_cstr(int16_t n, char* s, const int len);
 
-  void updateQueueSizeFile();
+  void updateHeadFile();
+  void updateTailFile();
+  int16_t getHeadFromFile();
+  int16_t getTailFromFile();
+  void initializeHeadTail();
   
 };
 
